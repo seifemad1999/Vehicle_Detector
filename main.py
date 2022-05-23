716d1d1a89a457a2068dc4dcec8e66ea42fc8cc8
seif-adel <seif.arabesque@gmail.com>
2022-05-23 05:46:39 -0700

    Hog visualization
    classifier
    data preparation

--- main.py
+++ main.py
@@ -154,7 +154,31 @@ def extract_features(imgs, cspace='RGB', orient=9,
     return features
 
 
+###############################################HOG Visualisation
+car_test = mpimg.imread(car_images[35])
+car_test = cv2.cvtColor(car_test, cv2.COLOR_RGB2YCrCb)
+non_car_test = mpimg.imread(non_car_images[20])
+non_car_test = cv2.cvtColor(non_car_test, cv2.COLOR_RGB2YCrCb)
 
+imgs = []
+titles = []
+for i in range(3):
+    for feature_image, img_type in zip([car_test, non_car_test], ['Car', 'Non-car']):
+        channel = feature_image[:,:,i]
+        imgs.append(channel)
+        titles.append(img_type + ' CH%d' % i)
+        features, hog_image = get_hog_features(channel, orient=12, pix_per_cell=8, cell_per_block=2, 
+                        vis=True, feature_vec=False)
+        imgs.append(hog_image)
+        titles.append(img_type + ' CH%d' % i + ' HOG')
+        
+fig, axes = plt.subplots(3, 4, sharex=True, sharey=True, figsize=(14, 10))
+axes = axes.ravel()
+for ax, img, title in zip(axes, imgs, titles):
+    ax.imshow(img, cmap='Greys_r')
+    ax.set_title(title)
+    ax.axis('off')
+plt.savefig('output_images/HOG_comparison.png')
 
 ########################################### Feature Extraction
 ### Parameters
@@ -180,6 +204,48 @@ non_car_features = extract_features(non_car_images,cspace=colorspace,orient=orie
                         hist_bins=hist_bins, hist_range=(0, 256))
 
 
+#######################################Data Preparation
+# Preserve State to compare parameter tuning 
+rand_state = np.random.randint(0, 100)
+
+# Create an array stack of feature vectors
+X = np.vstack((car_features, non_car_features)).astype(np.float64)                        
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(non_car_features))))
+
+# Split up data into randomized training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using spatial binning of:',spatial,'and', hist_bins,'histogram bins')
+print('Feature vector length:', len(X_train[0]))
+
+###########################################Classifier
+# Use a linear SVC 
+svc = LinearSVC()
+
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+
+########################################### Prediction 
+# Check the prediction time for a single sample
+t=time.time()
+n_predict = 10
+print('My SVC predicts:     ', svc.predict(X_test[0:n_predict]))
+print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+t2 = time.time()
+print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
 
 
 
